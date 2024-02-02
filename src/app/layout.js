6d88ac38adcc0d84c52e765462cb316bd9413105
II-Paulus-II <paulus.coding@gmail.com>
2024-02-02 14:22:17 +0000

    added clerk, sign in and sign out pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,9 @@
+/* ----- Global Imports ----- */ 
+import Link from "next/link";
+import { ClerkProvider, UserButton, auth, currentUser } from "@clerk/nextjs";
+import { sql } from "@vercel/postgres";
+
+/* ----- Project Imports ----- */
 import "./globals.css";
 
 export const metadata = {
@@ -5,10 +11,16 @@ export const metadata = {
   description: "Friendly anti-social social media website",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const { userId } = auth();
+  const user = await currentUser();
   return (
+    <ClerkProvider>
     <html lang="en">
+      {userId && <UserButton afterSignOutUrl="/" />}
+      {!userId && <Link href="/sign-in">Sign In</Link>}
       <body>{children}</body>
     </html>
+    </ClerkProvider>
   );
-}
+};
