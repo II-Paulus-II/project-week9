454ac9994a3c24067fe21f64fef3041e762de771
Paul Hughes <paulus.coding@gmail.com>
2024-04-22 20:15:42 +0100

    added login section layout

---
+++ src/app/(login)/layout.jsx
@@ -0,0 +1,16 @@
+/* ----- Third Party Imports ----- */ 
+
+/* ----- Project Imports ----- */
+import "@styles/login.css";
+
+
+export default function RootLayout({ children }) {
+
+  return (
+    <>
+      <main className="loginMainElement">
+      {children}
+      </main>
+    </>
+  );
+};
