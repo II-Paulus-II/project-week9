957f2f61b1f010340b3dc481887c4b345ce9bef9
Paul Hughes <paulus.coding@gmail.com>
2024-04-26 16:32:52 +0100

    tidied up navbar slightly

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,12 +1,16 @@
 /* ----- Global Imports ----- */ 
 import Link from "next/link";
 
+/* ----- Project Imports ----- */
+import "@/components/nav/nav.css";
+
 export default function Nav() {
   return (
     <nav className="navigation">
-      <Link href="/faq" >FAQ</Link>
-      <Link href="/users" >Users</Link>
-      <Link href="/recent" >Recent</Link>
+      <Link class="navLinks" href="/" >Home</Link>
+      <Link class="navLinks" href="/faq" >FAQ</Link>
+      <Link class="navLinks" href="/users" >Users</Link>
+      <Link class="navLinks" href="/recent" >Recent</Link>
     </nav>
   );
 }
